be5cf0f82fcbebf4de84bbfa118a4b44e9768627
Fernando Robles Rivera <fernando.robles@elektra.com.mx>
2020-03-11 17:41:03 -0600

    cambios en app de mailings

--- menu-emails/src/App.js
+++ menu-emails/src/App.js
@@ -1,68 +1,53 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Menu from "./views/Menu";
 import styles from "./index.module.sass";
-import path from "../src/views/misc/path";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  const [resolution, setresolution] = useState(650);
+export default function App() {
+  const [resolution, setResolution] = useState(650);
+  function handleChangeResolution(e) {
+    setResolution(~~e.target.value);
+  }
 
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <div className={styles.container}>
-            <Menu />
+      <div className={styles.container}>
+        <Menu />
+        <div className={styles.content}>
+          <div className={styles.containerEmail}>
+            <div>
+              <select id={styles.devices} onChange={handleChangeResolution}>
+                <option value="650">Selecciona una resolución</option>
+                <option value="320">320 iPhone 5/SE</option>
+                <option value="360">360 Galaxy S5</option>
+                <option value="375">375 iPhone 6/7/8/X</option>
+                <option value="411">411 Pixel 2</option>
+                <option value="414">414 iPhone 6/7/8 Plus</option>
+                <option value="768">768 iPad</option>
+                <option value="650">650</option>
+                <option value="414">414</option>
+              </select>
+            </div>
           </div>
-        </Route>
-
-        {path.map((val, key) => {
-          return (
-            <Route path={val.url} key={key}>
-              <div className={styles.container}>
-                <Menu keyMenu={key} />
-                <div className={styles.content}>
-                  <div className={styles.containerEmail}>
-                    <div
-                      onClick={() => {
-                        setresolution(650);
-                      }}
-                      className={`material-icons ${styles.icon}
-                        ${(resolution === 650 && styles.tab_active) ||
-                          styles.tab}
-                      `}
-                    >
-                      desktop_mac
-                    </div>
-
-                    <div
-                      onClick={() => {
-                        setresolution(414);
-                      }}
-                      className={`material-icons ${styles.icon}
-                      ${(resolution === 414 && styles.tab_active) || styles.tab}
-                    `}
-                    >
-                      mobile_friendly
-                    </div>
-                  </div>
-                  <div className={styles.cointainerIframe}>
-                    <iframe
-                      src={val.src}
-                      width={`${resolution}px`}
-                      height="1400"
-                      frameBorder="0"
-                      title="render"
-                    ></iframe>
-                  </div>
-                </div>
-              </div>
-            </Route>
-          );
-        })}
-      </Switch>
+          <div className={styles.cointainerIframe}>
+            <Route
+              path="/htmls/:source"
+              render={({ match: { params } }) => {
+                return (
+                  <iframe
+                    height="1400"
+                    title="render"
+                    frameBorder="0"
+                    width={`${resolution}px`}
+                    src={`/${params.source}/index.html`}
+                  />
+                );
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </Router>
   );
 }
-
-export default App;
