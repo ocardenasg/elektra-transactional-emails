312cafdfa10befba772cd08b364afe1f2d035672
ocardenasg <ojcardenas@outlook.com>
2020-04-27 17:38:44 -0500

    Updates

---
+++ precache-manifest.4b9b466d9eda500d0d8c361cb5b1efcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2de2a61e3f49540ffd57455805c4a0",
+    "url": "/elektra-transactional-emails/index.html"
+  },
+  {
+    "revision": "ec7b7d8c036ddf81949f",
+    "url": "/elektra-transactional-emails/static/css/main.f31e4a71.chunk.css"
+  },
+  {
+    "revision": "37794598b3161291feb7",
+    "url": "/elektra-transactional-emails/static/js/2.77a88206.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/elektra-transactional-emails/static/js/2.77a88206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7b7d8c036ddf81949f",
+    "url": "/elektra-transactional-emails/static/js/main.36147204.chunk.js"
+  },
+  {
+    "revision": "deae9c227d1888109cc9",
+    "url": "/elektra-transactional-emails/static/js/runtime-main.917ca45e.js"
+  }
+]);
--- src/views/PedidoCreado/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderHTML from "react-render-html";
-
-const someHTML = "<div></div>";
-const PedidoCreado = () => {
-  return <div>{renderHTML(someHTML)}</div>;
-};
-export default PedidoCreado;
